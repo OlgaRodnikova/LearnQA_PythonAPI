68f2cbf899e8cfb42e64304cf7fd5fda519da589
Olga_Rodnikova <o.v.rodnikova@gmail.com>
2024-11-24 16:34:05 +0300

    Add files

---
+++ ex6_redirect.py
@@ -0,0 +1,9 @@
+import requests
+
+response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+
+number_of_redirects = len(response.history)
+print(f"Количество редиректов в запросе равно {number_of_redirects}")
+
+url_final = response.url
+print(f"Итоговый URL {url_final}")
